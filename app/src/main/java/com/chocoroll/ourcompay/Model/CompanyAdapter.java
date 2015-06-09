6c62fb242c36dee6b2a50081a7c1e095da0d8422
redabraxas <udhee94@nate.com>
2015-06-09 17:49:06 +0900

    dan

--- app/src/main/java/com/chocoroll/ourcompay/Model/CompanyAdapter.java
+++ app/src/main/java/com/chocoroll/ourcompay/Model/CompanyAdapter.java
@@ -12,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.chocoroll.ourcompay.Extra.DownloadImageTask;
 import com.chocoroll.ourcompay.R;
 
 import java.io.InputStream;
@@ -47,28 +48,4 @@ public class CompanyAdapter extends ArrayAdapter<Company> {
         return v;
     }
 
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
-    }
 }
